57b28fc821fbb30ec4dae8816d2e0744d12d4fab
Rodrigo Moreno <luiromormor@gmail.com>
2021-11-25 22:31:05 -0600

    Initial commit

---
+++ script.py
@@ -0,0 +1,102 @@
+import requests
+from bs4 import BeautifulSoup
+
+surl = 'https://www.imdb.com/find?q={}&ref_=nv_sr_sm'
+
+
+class Actor():
+    def __init__(self, code):
+        self.url = 'https://www.imdb.com/name/{}/?ref_=fn_al_nm_1'.format(code)
+        headers = {'Accept-Language': 'en-US'}
+        self.resp = requests.get(self.url, headers = headers)
+        self.soup = BeautifulSoup(self.resp.content, 'html.parser')
+        #self.name = name
+        self.extract_movies()
+
+    def extract_movies(self):
+        """
+        Extract the movies of the actor.
+        """
+        resp = self.soup.find(class_ = 'filmo-category-section')
+        jobs = resp.find_all('b')
+        #print(self.job_urls)
+
+        # This kind of fails because it doesn't filter TV shows away
+        self.job_names = [job.text for job in jobs]
+        #print(self.job_names)
+        self.job_urls = [job.find('a')['href'].split('/')[-2] for job in jobs]
+        self.jobs = dict(zip(self.job_urls, self.job_names))
+        return self.jobs
+
+
+class Movie():
+    def __init__(self, code):
+        self.url = 'https://www.imdb.com/title/{}/?ref_=fn_al_nm_1'.format(code)
+        headers = {'Accept-Language': 'en-US'} # This could probably be a global variable
+        self.resp = requests.get(self.url, headers = headers)
+        self.soup = BeautifulSoup(self.resp.content, 'html.parser')
+        #self.name = name
+        self.get_cast()
+
+    def _get_cast_link(self):
+        """
+        The full cast is on another page. It needs to be found and followed to
+        get the information.
+        """
+        clabel = ('ipc-metadata-list-item__label'
+                  ' ipc-metadata-list-item__label--link')
+        link = self.soup.find(class_ = clabel)
+        return link['href']
+
+    def get_cast(self):
+        cast = Cast(self._get_cast_link())
+        cast.get_cast()
+        self.cast_urls = cast.cast_urls
+        self.cast_names = cast.cast_names
+        self.cast = dict(zip(self.cast_urls, self.cast_names))
+        return self.cast
+
+
+class Cast():
+    def __init__(self, code):
+        self.url = 'http://www.imdb.com{}'.format(code)
+        headers = {'Accept-Language': 'en-US'}
+        self.resp = requests.get(self.url, headers = headers)
+        self.soup = BeautifulSoup(self.resp.content, 'html.parser')
+
+    def get_cast(self):
+        """
+        Get list of cast members and URL 'constructors'.
+        """
+        h = self.soup.find(class_ = 'cast_list')
+        cast_list = h.find_all(class_ = 'primary_photo')
+        self.cast_urls = [actor.find('a')['href'].split('/')[-2]
+                          for actor in cast_list]
+        self.cast_names = [actor.find('img')['title'] for actor in cast_list]
+        self.cast = dict(zip(self.cast_urls, self.cast_names))
+
+
+def search_imdb(query):
+    """
+    Search a query on imbd.
+    """
+    query = query.split()
+    query = '+'.join(query)
+    resp = requests.get(surl.format(query))
+    print(resp.headers)
+
+
+
+if __name__ == '__main__':
+    a = Actor('nm0001548')
+    print(a.resp)
+    print(a.job_names)
+    print(a.job_urls)
+    
+    print('')
+
+    m = Movie('tt0098067')
+    print(m.resp)
+    m._get_cast_link()
+    print(m.cast_names)
+    print(m.cast_urls)
