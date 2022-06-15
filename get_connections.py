2a1fc139b1b4f1aec3dc77b726cd99df66b0c390
Rodrigo Moreno <luiromormor@gmail.com>
2022-06-15 14:13:04 -0500

    Added function to sort the movies with which there is a connection. Also added script and function definitions.

--- get_connections.py
+++ get_connections.py
@@ -1,3 +1,7 @@
+"""
+A script that finds the connections of a movie and sorts them according to
+their ranking.
+"""
 from movies import *
 from requests import Session
 import cProfile
@@ -8,6 +12,15 @@ import asyncio
 
 
 async def get_connections(movie):
+    """
+    Get a set of movie codes that connect with a given movie.
+
+    Input:
+    - movie: A movie code from which a movies.Movie() object can be created.
+
+    Outpt:
+    - c: a set() of connections.
+    """
     movie = Movie(movie)
     movie.get_cast()
     print(movie.name)
@@ -24,20 +37,61 @@ async def get_connections(movie):
 
     actors = [Actor(html = resp.content) for resp in reqs]
     for actor in actors:
-        print(actor.name)
+        #print(actor.name)
         actor.extract_movies()
         for k, v in actor.jobs.items():
-            c.add((k, v))
+            c.add(k)
 
-    print(c)
+    return c
+
+
+async def sort_connections(movie_set):
+    """
+    Sort a set of movie codes according to their movie scores in IMDB.
+
+    Input:
+    - movie_set: A set of movie codes.
+
+    Output:
+    - None
+    """
+    url = 'https://www.imdb.com/title/{}/?req_=fn_al_1'
+    header = {'Accept-Language': 'en-US'}
+    movie_set = list(movie_set)
+    movies = []
+    ii = 1
+
+    while movie_set:
+        print(f'On round {ii}.')
+        cs = 10
+        cms = movie_set[:cs]
+        del movie_set[:cs]
+        async with httpx.AsyncClient() as client:
+            tasks = (client.get(url.format(code), headers = header) for code in cms)
+            reqs = await asyncio.gather(*tasks)
+        cmovies = [Movie(html = resp.content) for resp in reqs]
+        movies.extend(cmovies)
+        ii += 1
+    print('Finished making movies.')
+
+    #movies = [Movie(html = resp.content) for resp in reqs]
+    for movie in movies:
+        #print(movie.name)
+        movie.get_score()
+
+    movies.sort()
+    print(movies)
+    return None
 
 
 def main():
     movie = sys.argv[1]
-    
-    with cProfile.Profile() as pr:
-        asyncio.run(get_connections(movie))
 
+    with cProfile.Profile() as pr:
+        conn_set = asyncio.run(get_connections(movie))
+        #conn_set = set(list(conn_set)[:10])
+        asyncio.run(sort_connections(conn_set))
+    
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
     stats.dump_stats(filename = 'conn.prof')
