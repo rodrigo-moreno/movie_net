c6dac396d26a8deff07bd053eb828e66e20c7624
Rodrigo Moreno <luiromormor@gmail.com>
2022-06-15 11:15:10 -0500

    Changes made to display movie titles (to confirm the codes are correct) and to display actor names instead of codes.

--- cast_intersection.py
+++ cast_intersection.py
@@ -6,14 +6,17 @@ def cast_intersection(c1, c2):
     Gets the cast intersection of two movies defined by their codes.
     '''
     m1 = Movie(c1)
+    print(m1)
     m1.get_cast()
     m2 = Movie(c2)
+    print(m2)
     m2.get_cast()
 
     s1 = set(m1.cast)
     s2 = set(m2.cast)
 
     inter = s1 & s2
+    inter = [Actor(a) for a in inter]
     return inter
 
 
